de869da9bdaae12f79693b8ba35b983456c507fa
Jorge Ramirez <joramirez96@gmail.com>
2020-04-01 00:37:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize}:"
-  
+
       array = holiday.to_s.split("_").map
       final_holiday = []
       array.each do |i|
