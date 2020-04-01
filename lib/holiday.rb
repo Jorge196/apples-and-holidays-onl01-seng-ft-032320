cdc5d57469689f09c32fd540a5af095c6121acae
Jorge Ramirez <joramirez96@gmail.com>
2020-04-01 01:12:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,10 +70,9 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-    answer = []
-    holiday_hash.collect{ |season, data|
-        holiday.select{ |holiday, supply|
-          supplies.include?("BBQ")
-        }.keys
-      }.flatten
+    holiday_hash.map do |season, holiday| 
+      holiday.map do |holiday, item|
+        holiday if item.include?("BBQ")
+      end 
+    end.flatten.compact 
 end
